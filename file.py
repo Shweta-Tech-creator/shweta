440ec8919eaa2b0a4d62d5f3eb665da75d06fa70
shweta16 <kadamsweta92@gmail.com>
2025-01-25 14:23:55 +0530

    updated new

---
+++ file.py
@@ -0,0 +1,15 @@
+def stringlength(s):
+    count=0
+    for range in s:
+        count +=1
+        return count
+string=input("enter a string:")
+print("lenght of the string is:",stringlength(string)) 
+# changes by done by shweta
+
+
+
+
+
+
+
